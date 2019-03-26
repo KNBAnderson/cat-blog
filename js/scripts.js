b8e9b780ed627ab0bfe40c5a571c22e3183b2494
Katlin Anderson and Mathew Akre <mathew@>
2019-03-26 13:31:12 -0700

    add hide to cat-containers so they show one at a time when thier buttons are clicked

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,23 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("lightblue-background");
     $("#sparkles").toggle();
+    $("#bubbles").hide();
+    $("#sully").hide();
   });
 
   $("button#darkblue").click(function() {
     $("body").removeClass();
     $("body").addClass("darkblue");
     $("#bubbles").toggle();
+    $("#sparkles").hide();
+    $("#sully").hide();
   });
 
   $("button#orange").click(function() {
     $("body").removeClass();
     $("body").addClass("orange");
     $("#sully").toggle();
+    $("#sparkles").hide();
+    $("#bubbles").hide();
   });
 });
